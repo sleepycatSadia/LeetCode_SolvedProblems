a54d218c2ef25fc587ef87a174e88c09afebe2fc
Sadia Tasnim <56369850+sleepycatSadia@users.noreply.github.com>
2022-05-20 12:04:49 +0600

    Time: 19 ms (82.65%), Space: 13.5 MB (17.35%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def xorOperation(self, n, start):
+        """
+        :type n: int
+        :type start: int
+        :rtype: int
+        """
+        
+        r=start
+    
+        for i in range(1,n):
+            r=r^(start+2*i)
+        return r

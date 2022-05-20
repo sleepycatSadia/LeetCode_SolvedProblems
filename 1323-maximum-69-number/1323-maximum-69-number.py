e84040a26d8b326f85e03f2f315654b817036c88
Sadia Tasnim <56369850+sleepycatSadia@users.noreply.github.com>
2022-05-20 22:47:37 +0600

    Time: 28 ms (22.00%), Space: 13.3 MB (71.33%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -4,9 +4,4 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        num=[i for i in str(num)]
-        for i in range(len(num)):
-            if num[i] == '6':
-                num[i]='9'
-                break
-        return int(''.join(num))
+        return int(str(num).replace('6','9',1))

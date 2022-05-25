780886a5c65a4975b7316b2fe6731a2461e60a4d
Sadia Tasnim <56369850+sleepycatSadia@users.noreply.github.com>
2022-05-25 18:16:37 +0600

    Time: 56 ms (6.01%), Space: 13.5 MB (69.92%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        d={'a':".-",'b':"-...",'c':"-.-.",'d':"-..",'e':".",'f':"..-.", 'g':"--.",'h':"....",
+           'i':"..",'j':".---",'k':"-.-",'l':".-..",'m':"--",'n':"-.", 'o':"---",'p':".--.",
+           'q':"--.-",'r':".-.",'s':"...",'t':"-",'u':"..-",'v':"...-",'w':".--",'x':"-..-",'y':"-.--",'z':"--.."}
+        for idx in range(len(words)):
+            s=''
+            for i in words[idx] :
+                s+=d.get(i)
+            words[idx]=s
+        return len(set(words))

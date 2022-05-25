ceb75d82645dff52d6430f06362fed98da1b2a8e
Sadia Tasnim <56369850+sleepycatSadia@users.noreply.github.com>
2022-05-25 20:00:17 +0600

    Time: 92 ms (34.95%), Space: 13.7 MB (34.07%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        x,y=0,0
+        for i in moves:
+            if i == 'U' :
+                y+=1
+            elif i == 'D':
+                y-=1
+            elif i == 'L':
+                x-=1
+            else:
+                x+=1
+        return x==0 and y ==0

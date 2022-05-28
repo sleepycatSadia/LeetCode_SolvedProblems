f8dad029d5b0c06c233fae9166d69be260650076
Sadia Tasnim <56369850+sleepycatSadia@users.noreply.github.com>
2022-05-29 00:13:03 +0600

    Time: 28 ms (87.05%), Space: 14.3 MB (16.14%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        lst=[]
+        for i in range(1,n+1):
+            if i %3==0 and i %5==0:
+                lst.append('FizzBuzz')
+            elif (i %3 ==0):
+                lst.append('Fizz')
+            elif (i %5 ==0):
+                lst.append('Buzz')
+            else:
+                lst.append(str(i))
+        return lst
+        

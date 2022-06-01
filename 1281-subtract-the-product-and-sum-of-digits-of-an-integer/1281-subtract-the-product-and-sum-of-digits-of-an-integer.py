0bdff8af417bb5607a99c781590b13714ec09abf
Sadia Tasnim <56369850+sleepycatSadia@users.noreply.github.com>
2022-06-01 18:38:18 +0600

    Time: 48 ms (34.16%), Space: 13.8 MB (53.70%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -1,37 +1,9 @@
-class Solution(object):
-    def subtractProductAndSum(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        
         n=list(map(int, str(n)))
         p=1
         for i in n:
             
             p*=int(i)
-        return p-sum(n)    
-'''
-class Solution(object):
-    def subtractProductAndSum(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        s,p=0,1
-        for i in list(str(n)):
-            s+=int(i)
-            p*=int(i)
-        return p-s    
- class Solution(object):
-    def subtractProductAndSum(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        n=[int(i) for i in (str(n))]
-        p=1
-        for i in n:
-            
-            p*=int(i)
-        return p-sum(n)
-'''
+        return p-sum(n) 
